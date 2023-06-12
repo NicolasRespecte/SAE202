846a3cb2c19ae11200e63856d4177449afbd612c
NicolasRespecte <126659931+NicolasRespecte@users.noreply.github.com>
2023-06-12 11:46:12 +0200

    blanc
    blanc logo

--- header.inc.php
+++ header.inc.php
@@ -1,19 +1,10 @@
 <link rel="stylesheet" href="../assets/css/styles.css">
-<<<<<<< Updated upstream
   <meta name="viewport" content="width=device-width, initial-scale=1">
     <nav class="top-nav">
       <div class="logos">
-        <a href="../index.php"><img class="logo" src="../assets/imgs/logosolo.svg" alt=""></a>
-        <a href="../index.php"><img class="titrelogo" src="../assets/imgs/logotexte.svg" alt="" ></a>
+        <a href="index.php"><img class="logo" src="../assets/imgs/logosolo.svg" alt=""></a>
+        <a href="index.php"><img class="titrelogo" src="../assets/imgs/textelogo.png" alt="" ></a>
       </div>
-=======
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <nav class="top-nav">
-    <div class="logo">
-      <a href="index.php"><img src="../assets/imgs/logosolo.svg" alt=""></a>
-      <a href="index.php"><img src="../assets/imgs/logotext.svg" alt=""></a>
-    </div>
->>>>>>> Stashed changes
     <input id="menu-toggle" type="checkbox" />
     <label class='menu-button-container' for="menu-toggle">
     <div class='menu-button'></div>
@@ -28,10 +19,10 @@
       <?php
   
         if (isset($_SESSION['prenom_usager'])){
-        echo '<div class="co"><a href="../profil.php">'.$_SESSION['prenom_usager'].'</a> / ';
-        echo '<a href="../deconnexion.php">Déconnexion</a></div>';
+        echo '<div class="co"><a href="profil.php">'.$_SESSION['prenom_usager'].'</a> / ';
+        echo '<a href="deconnexion.php">Déconnexion</a></div>';
         } else {
-            echo '<div class="pasco"><a href="../ormConnexion.php">Connexion</a></div>';
+            echo '<div class="pasco"><a href="formConnexion.php">Connexion</a></div>';
         } 
         
       ?>
