9dcb3d15f5ba00eb644b366ac8409d5b7fce5fd6
YanisKujo <126659730+YanisKujo@users.noreply.github.com>
2023-06-13 10:39:40 +0200

    fff
    d

--- header.inc.php
+++ header.inc.php
@@ -1,10 +1,19 @@
 <link rel="stylesheet" href="../assets/css/styles.css">
+<<<<<<< Updated upstream
   <meta name="viewport" content="width=device-width, initial-scale=1">
     <nav class="top-nav">
       <div class="logos">
         <a href="index.php"><img class="logo" src="../assets/imgs/logosolo.svg" alt=""></a>
         <a href="index.php"><img class="titrelogo" src="../assets/imgs/textelogo.png" alt="" ></a>
       </div>
+=======
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <nav class="top-nav">
+    <div class="logo">
+      <a href="index.php"><img src="../assets/imgs/logosolo.svg" alt=""></a>
+      <a href="index.php"><img src="../assets/imgs/logotext.svg" alt=""></a>
+    </div>
+>>>>>>> Stashed changes
     <input id="menu-toggle" type="checkbox" />
     <label class='menu-button-container' for="menu-toggle">
     <div class='menu-button'></div>
